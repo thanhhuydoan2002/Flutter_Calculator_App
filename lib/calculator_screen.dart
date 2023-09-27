cf6fe9929df48bdee479d1de017ed6337eee14fd
thanhhuydoan2002 <ddoanthanhhuy@gmail.com>
2023-09-27 21:07:10 +0700

    button color and button layout updated

--- lib/calculator_screen.dart
+++ lib/calculator_screen.dart
@@ -1,3 +1,4 @@
+import 'package:calculator/button_values.dart';
 import 'package:flutter/material.dart';
 
 class CalculatorScreen extends StatefulWidget {
@@ -10,6 +11,8 @@ class CalculatorScreen extends StatefulWidget {
 class _CalculatorScreenState extends State<CalculatorScreen> {
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+
     return Scaffold(
       body: SafeArea(
         bottom: false,
@@ -18,6 +21,7 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
             //output
             Expanded(
               child: SingleChildScrollView(
+                reverse: true,
                 child: Container(
                   alignment: Alignment.bottomRight,
                   padding: const EdgeInsets.all(16),
@@ -34,10 +38,68 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
             ),
 
             //buttons
+            Wrap(
+              children: Btn.buttonValues
+                  .map(
+                    (value) => SizedBox(
+                      width: value==Btn.n0
+                      ? screenSize.width/2
+                      : (screenSize.width / 4),
+                      height: screenSize.width / 5,
+                      child: buildButton(value)
+                    ),
+                  )
+                  .toList(),
+            ),
 
           ],
         ),
       ),
     );
   }
+
+  Widget buildButton(value){
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Material(
+        //Button Colors
+        color: getBtnColor(value),
+        clipBehavior: Clip.hardEdge,
+        shape: OutlineInputBorder(
+          borderSide: const BorderSide(
+            color: Colors.white24,
+          ),
+          borderRadius: BorderRadius.circular(100),
+        ),
+        child: InkWell(
+          onTap: () {},
+          child: Center(
+            child: Text(
+              value, 
+              style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+        ),
+      ),
+    );
+  }
+
+  Color getBtnColor(value){
+    return [Btn.del,Btn.clr].contains(value)?
+        Colors.blueGrey
+        : [
+            Btn.per,
+            Btn.multiply,
+            Btn.add,
+            Btn.subtract,
+            Btn.divide,
+            Btn.calculate,
+          ].contains(value)?
+        Colors.orange
+        : Colors.black87;
+  }
+
 }
